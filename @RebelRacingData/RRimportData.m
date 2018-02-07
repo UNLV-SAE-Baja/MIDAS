3a34b6d125a6f4abe60164540886bfe1c4306e59
Tristan Ayala <ayalat1@unlv.nevada.edu>
2018-02-06 17:40:24 -0800

    Changed RRimportData by expanding array of variable names
    Updated/Upgraded the old system of cleaning variable names. Still need to add 'space' support, this is an easy fix.

--- @RebelRacingData/RRimportData.m
+++ @RebelRacingData/RRimportData.m
@@ -13,64 +13,72 @@
 %   RRdataTable.Properties.VariableUnits
 
 function RRimportData(obj, filename)
-%% Introduction
+%% Preliminary
     %Check whether or not input file is char array
     if (~ischar(filename))
         error("Error: Not a file");
     end
             
-    %Create temporary dataset
-    tempData = importdata(filename, ",", 1);
-            
-    %Preallocate empty cell array to size of the input file 
-    %in order to save variable units
-    unitArray = cell(length(tempData.textdata),1);
+    %Import the file into memory with readTable
+    obj.RRdataTable = readtable(fullfile(obj.RRtempFileDirectory,filename));
 
 %% Move Imported Data to RRdataTable
-
-    obj.RRdataTable = array2table(tempData.data);
     
     %Set description of the RRdataTable
-    obj.RRdataTable.Properties.Description = ['Data Table Generated From ',filename]; 
+    obj.RRdataTable.Properties.Description = ['Data Table Generated From ',filename,'on',date]; 
     
 %% Variable Names and Units
-     for i = 1:length(tempData.textdata)
+
+    tempVar = obj.RRdataTable.Properties.VariableNames;
+    
+    for i = 1:length(tempVar)
     
         cnt = 0;
         strtPos = 0;
-        endPos = 0; %#ok<NASGU>
+        endPos = 0;
         columnName = ''; %#ok<NASGU>
 
-        cellContents = tempData.textdata{i};
+        cellContents = tempVar{i};
                 
-        for j = 1:length(cellContents)
-            %Count the number of double quotes to separate the variable
-            %name from the variable units
-            if cellContents(j) == '"'
+        j = 1;
+        while j <= length(cellContents)
+
+            if (cellContents(j) <= 'z' && cellContents(j) >= 'a') || (cellContents(j) <= 'Z' && cellContents(j) >= 'A')
                 cnt = cnt + 1;
-                    
+                
                 switch cnt
                     case 1
-                        strtPos = j + 1; 
-                    
-                    case 2
+                        strtPos = j;
+                        while (cellContents(j) <= 'z' && cellContents(j) >= 'a') || (cellContents(j) <= 'Z' && cellContents(j) >= 'A') || (cellContents(j) >= '0' && cellContents(j) <= '9')
+                            j = j + 1;
+                        end
                         endPos = j - 1;
                         columnName = cellContents(strtPos:endPos);
                         obj.RRdataTable.Properties.VariableNames{i} = columnName;
-                    
-                    case 3
-                        strtPos = j + 1;
-                    
-                    case 4 %save units to the cell array
+                        
+                    case 2
+                        strtPos = j;
+                        while (cellContents(j) < 'z' && cellContents(j) > 'a') || (cellContents(j) < 'Z' && cellContents(j) > 'A') || (cellContents(j) > '0' && cellContents(j) < '9')
+                            j = j + 1;
+                        end
                         endPos = j - 1;
                         unitArray{i} = char(cellContents(strtPos:endPos));
-                        
+                           
                 end
             end
+            
+                %Check whether or not the Units were defined for the
+                %variable
+                if cnt < 2 && j == length(cellContents)
+                    unitArray{i} = '';
+                end
+                
+                j = j + 1;
+                
         end 
     end
      
     %Populate variable units information
     obj.RRdataTable.Properties.VariableUnits = unitArray;
-            
+        
 end
